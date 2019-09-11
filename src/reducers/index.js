7486c4ca0cf715c6bf37b9ee1a8c5b684381599b
Jason Sonnichsen <Harliquinn@gmail.com>
2019-09-11 18:35:02 -0400

    Forgot to restore price

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import { REM_ITEM } from "../actions";
 const initState = {
   additionalPrice: 0,
   car: {
-    price: 1000, //26395,
+    price: 26395,
     name: "2019 Ford Mustang",
     image:
       "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
